10149ed2a5e64a6c021bd3d553cce8a56de9b2ab
krishnaveer singh <85020105+adeveloperkk@users.noreply.github.com>
2023-01-24 11:07:01 +0530

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -1,7 +1,7 @@
 from webbot import Browser 
 web = Browser()
 web.go_to('https://lms.cuchd.in') 
-web.type('20BCS1509' , into='username')
+web.type('20BCSCXXXX' , into='username')
 web.click('NEXT' , tag='span')
-web.type('@52535455Pp' , into='password' , id='password') # specific selection
+web.type('pass@123' , into='password' , id='password') # specific selection
 web.click('submit' , tag='span') # you are logged in ^_^
